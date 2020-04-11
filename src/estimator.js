c592c34651cd1d9fcbf77d8ba0615beb3a4baceb
dmukuna <danielmukuna.k@outlook.com>
2020-04-11 22:19:07 +0300

    fixed challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -22,9 +22,9 @@ const covid19ImpactEstimator = (data) => {
   const infectionsByRequestedTimeSevere = currentlyInfectedSevere * (2 ** Math.trunc(days / 3));
   const severeCasesByRequestedTimeImpact = Math.trunc(0.15 * infectionsByRequestedTimeImpact);
   const severeCasesByRequestedTimeSevere = Math.trunc(0.15 * infectionsByRequestedTimeSevere);
-  const availableBeds = Math.trunc(0.35 * totalHospitalBeds);
-  const availableBedsImpact = availableBeds - severeCasesByRequestedTimeImpact;
-  const availableBedsSevere = availableBeds - severeCasesByRequestedTimeSevere;
+  const availableBeds = 0.35 * totalHospitalBeds;
+  const availableBedsImpact = Math.trunc(availableBeds - severeCasesByRequestedTimeImpact);
+  const availableBedsSevere = Math.trunc(availableBeds - severeCasesByRequestedTimeSevere);
   const icuCaseImpact = Math.trunc(0.05 * infectionsByRequestedTimeImpact);
   const icuCaseSevere = Math.trunc(0.05 * infectionsByRequestedTimeSevere);
   const ventCaseImpact = Math.trunc(0.02 * infectionsByRequestedTimeImpact);
